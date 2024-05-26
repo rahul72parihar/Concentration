7b0509fc97a229ebf399f03f796b52555e8fe56a
Rahul Parihar <rahul72parihar@gmail.com>
2024-05-26 07:16:13 +0530

    Initial Commit

---
+++ Concentration/ConcentrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcentrationApp.swift
+//  Concentration
+//
+//  Created by Rahul Parihar on 26/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ConcentrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Concentration/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Concentration
+//
+//  Created by Rahul Parihar on 26/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, worsdfsdfld!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
