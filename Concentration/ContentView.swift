e2f5770de3cf2070d349924a13c0c61323a44921
Rahul Parihar <rahul72parihar@gmail.com>
2024-05-26 07:22:04 +0530

    this is for new branch

--- Concentration/ContentView.swift
+++ Concentration/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, worsdfsdfld!")
+            Text("Hello, this would be on development branch!")
         }
         .padding()
     }
